ed30ecace8ad7ad8d85bfd74988dea2073e7d8f6
Kuba <kubalobo@gmail.com>
2021-01-13 09:15:42 +0100

    ServiceWorker file ready

---
+++ serviceWorker.js
@@ -0,0 +1,36 @@
+var CACHE_NAME = 'coffee-type';
+var urlsToCache = [
+  '/',
+  '/style.css',
+  '/index.html',
+  '/CoffeeTypes.html',
+  '/coffee1.jpg',
+  '/coffee_sketch.jpg'
+];
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
